8fdadb76ee6a65f13c0d9d706bccf1eb16039ee2
SuperStik <daviswood290@gmail.com>
2025-05-07 13:38:14 -0700

    Add option parsing

--- chatterbox.c
+++ chatterbox.c
@@ -2,12 +2,41 @@
 #include <err.h>
 #include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
 
 static void setupsignals(void);
+static void printhelp(const char *argv0);
 
 static char active = 1;
 
 int main(int argc, char **argv) {
+	char server = 0;
+	int opt;
+	const char *host = NULL;
+	const char *serv = NULL;
+	while((opt = getopt(argc, argv, "a:chp:s")) > -1 ) {
+		switch(opt) {
+			case 'a':
+				host = optarg;
+				break;
+			case 'c':
+				server = 0;
+				break;
+			case 'h':
+				printhelp(argv[0]);
+				return 0;
+			case 'p':
+				serv = optarg;
+				break;
+			case 's':
+				server = 1;
+				break;
+			default:
+				printhelp(argv[0]);
+				return 1;
+		}
+
+	}
 	setupsignals();
 	return 0;
 }
@@ -32,4 +61,9 @@ static void setupsignals(void) {
 
 	if (sigaction(SIGQUIT, &action, NULL))
 		err(2, "sigaction [L:%i F:%s]", __LINE__, __FILE__);
+}
+
+static void printhelp(const char *argv0) {
+	fprintf(stderr, "usage: %s [-h] [-c | -s] [-a host] [-p service]\n",
+			argv0);
 }
